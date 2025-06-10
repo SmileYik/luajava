c7eb1b45eeaa68cd468df7352be6a941ef0159fd
SmileYik <miskyle@outlook.com>
2025-06-10 10:50:01 +0800

    LuaStateFacade: wrapper

--- java/src/main/java/org/keplerproject/luajava/LuaStateFacade.java
+++ java/src/main/java/org/keplerproject/luajava/LuaStateFacade.java
@@ -289,15 +289,44 @@ public class LuaStateFacade implements AutoCloseable {
         }
     }
 
-    // PUSH FUNCTIONS
+    public int getTop() {
+        return lock(LuaState::getTop);
+    }
 
-    public LuaStateFacade toThread(int idx) {
-        lock.lock();
-        try {
-            return new LuaStateFacade(luaState.toThread(idx));
-        } finally {
-            lock.unlock();
-        }
+    public void setTop(int idx) {
+        lock(l -> {
+            l.setTop(idx);
+        });
+    }
+
+    public void pushValue(int idx) {
+        lock(l -> {
+            l.pushValue(idx);
+        });
+    }
+
+    public void remove(int idx) {
+        lock(l -> {
+            l.remove(idx);
+        });
+    }
+
+    public void insert(int idx) {
+        lock(l -> {
+            l.insert(idx);
+        });
+    }
+
+    public void replace(int idx) {
+        lock(l -> {
+            l.replace(idx);
+        });
+    }
+
+    public int checkStack(int sz) {
+        return lock(l -> {
+            return l.checkStack(sz);
+        });
     }
 
     // ACCESS FUNCTION
@@ -310,4 +339,610 @@ public class LuaStateFacade implements AutoCloseable {
             lock.unlock();
         }
     }
+
+    public boolean isNumber(int idx) {
+        return lock(l -> {
+            return l.isNumber(idx);
+        });
+    }
+
+    public boolean isString(int idx) {
+        return lock(l -> {
+            return l.isString(idx);
+        });
+    }
+
+    public boolean isFunction(int idx) {
+        return lock(l -> {
+            return l.isFunction(idx);
+        });
+    }
+
+    public boolean isCFunction(int idx) {
+        return lock(l -> {
+            return l.isCFunction(idx);
+        });
+    }
+
+    public boolean isUserdata(int idx) {
+        return lock(l -> {
+            return l.isUserdata(idx);
+        });
+    }
+
+    public boolean isTable(int idx) {
+        return lock(l -> {
+            return l.isTable(idx);
+        });
+    }
+
+    public boolean isBoolean(int idx) {
+        return lock(l -> {
+            return l.isBoolean(idx);
+        });
+    }
+
+    public boolean isNil(int idx) {
+        return lock(l -> {
+            return l.isNil(idx);
+        });
+    }
+
+    public boolean isThread(int idx) {
+        return lock(l -> {
+            return l.isThread(idx);
+        });
+    }
+
+    public boolean isNone(int idx) {
+        return lock(l -> {
+            return l.isNone(idx);
+        });
+    }
+
+    public boolean isNoneOrNil(int idx) {
+        return lock(l -> {
+            return l.isNoneOrNil(idx);
+        });
+    }
+
+    public int type(int idx) {
+        return lock(l -> {
+            return l.type(idx);
+        });
+    }
+
+    public String typeName(int tp) {
+        return lock(l -> {
+            return l.typeName(tp);
+        });
+    }
+
+    public int equal(int idx1, int idx2) {
+        return lock(l -> {
+            return l.equal(idx1, idx2);
+        });
+    }
+
+    public int rawequal(int idx1, int idx2) {
+        return lock(l -> {
+            return l.rawequal(idx1, idx2);
+        });
+    }
+
+    public int lessthan(int idx1, int idx2) {
+        return lock(l -> {
+            return l.lessthan(idx1, idx2);
+        });
+    }
+
+    public double toNumber(int idx) {
+        return lock(l -> {
+            return l.toNumber(idx);
+        });
+    }
+
+    public int toInteger(int idx) {
+        return lock(l -> {
+            return l.toInteger(idx);
+        });
+    }
+
+    public boolean toBoolean(int idx) {
+        return lock(l -> {
+            return l.toBoolean(idx);
+        });
+    }
+
+    public String toString(int idx) {
+        return lock(l -> {
+            return l.toString(idx);
+        });
+    }
+
+    public int strLen(int idx) {
+        return lock(l -> {
+            return l.strLen(idx);
+        });
+    }
+
+    public int objLen(int idx) {
+        return lock(l -> {
+            return l.objLen(idx);
+        });
+    }
+
+    // PUSH FUNCTIONS
+
+    public LuaStateFacade toThread(int idx) {
+        lock.lock();
+        try {
+            return new LuaStateFacade(luaState.toThread(idx));
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void pushNil() {
+        lock(LuaState::pushNil);
+    }
+
+    public void pushNumber(double db) {
+        lock(l -> {
+            l.pushNumber(db);
+        });
+    }
+
+    public void pushInteger(int integer) {
+        lock(l -> {
+            l.pushInteger(integer);
+        });
+    }
+
+    public void pushString(String str) {
+        lock(l -> {
+            l.pushString(str);
+        });
+    }
+
+    public void pushString(byte[] bytes) {
+        lock(l -> {
+            l.pushString(bytes);
+        });
+    }
+
+    // GET FUNCTIONS
+
+    public void pushBoolean(boolean bool) {
+        lock(l -> {
+            l.pushBoolean(bool);
+        });
+    }
+
+    public void getTable(int idx) {
+        lock(l -> {
+            l.getTable(idx);
+        });
+    }
+
+    public void getField(int idx, String k) {
+        lock(l -> {
+            l.getField(idx, k);
+        });
+    }
+
+    public void rawGet(int idx) {
+        lock(l -> {
+            l.rawGet(idx);
+        });
+    }
+
+    public void rawGetI(int idx, int n) {
+        lock(l -> {
+            l.rawGetI(idx, n);
+        });
+    }
+
+    public void createTable(int narr, int nrec) {
+        lock(l -> {
+            l.createTable(narr, nrec);
+        });
+    }
+
+    public void newTable() {
+        lock(LuaState::newTable);
+    }
+
+    /**
+     *
+     * @param idx
+     * @return if returns 0, there is no metatable
+     */
+    public int getMetaTable(int idx) {
+        return lock(l -> {
+            return l.getMetaTable(idx);
+        });
+    }
+
+    // SET FUNCTIONS
+
+    public void getFEnv(int idx) {
+        lock(l -> {
+            l.getFEnv(idx);
+        });
+    }
+
+    public void setTable(int idx) {
+        lock(l -> {
+            l.setTable(idx);
+        });
+    }
+
+    public void setField(int idx, String k) {
+        lock(l -> {
+            l.setField(idx, k);
+        });
+    }
+
+    public void rawSet(int idx) {
+        lock(l -> {
+            l.rawSet(idx);
+        });
+    }
+
+    public void rawSetI(int idx, int n) {
+        lock(l -> {
+            l.rawSetI(idx, n);
+        });
+    }
+
+    // if returns 0, cannot set the metatable to the given object
+    public int setMetaTable(int idx) {
+        return lock(l -> {
+            return l.setMetaTable(idx);
+        });
+    }
+
+    // if object is not a function returns 0
+    public int setFEnv(int idx) {
+        return lock(l -> {
+            return l.setFEnv(idx);
+        });
+    }
+
+    public void call(int nArgs, int nResults) {
+        lock(l -> {
+            l.call(nArgs, nResults);
+        });
+    }
+
+    // returns 0 if ok of one of the error codes defined
+    public int pcall(int nArgs, int nResults, int errFunc) {
+        return lock(l -> {
+            return l.pcall(nArgs, nResults, errFunc);
+        });
+    }
+
+    public int yield(int nResults) {
+        return lock(l -> {
+            return l.yield(nResults);
+        });
+    }
+
+    public int resume(int nArgs) {
+        return lock(l -> {
+            return l.resume(nArgs);
+        });
+    }
+
+    public int status() {
+        return lock(LuaState::status);
+    }
+
+    public int gc(int what, int data) {
+        return lock(l -> {
+            return l.gc(what, data);
+        });
+    }
+
+    public int getGcCount() {
+        return lock(LuaState::getGcCount);
+    }
+
+    public int next(int idx) {
+        return lock(l -> {
+            return l.next(idx);
+        });
+    }
+
+    public int error() {
+        return lock(LuaState::error);
+    }
+
+    public void concat(int n) {
+        lock(l -> {
+            l.concat(n);
+        });
+    }
+
+    // returns 0 if ok
+    public int LdoFile(String fileName) {
+        return lock(l -> {
+            return l.LdoFile(fileName);
+        });
+    }
+
+    // FUNCTION FROM lauxlib
+    // returns 0 if ok
+    public int LdoString(String str) {
+        return lock(l -> {
+            return l.LdoString(str);
+        });
+    }
+
+    public int LgetMetaField(int obj, String e) {
+        return lock(l -> {
+            return l.LgetMetaField(obj, e);
+        });
+    }
+
+    public int LcallMeta(int obj, String e) {
+        return lock(l -> {
+            return l.LcallMeta(obj, e);
+        });
+    }
+
+    public int Ltyperror(int nArg, String tName) {
+        return lock(l -> {
+            return l.Ltyperror(nArg, tName);
+        });
+    }
+
+    public int LargError(int numArg, String extraMsg) {
+        return lock(l -> {
+            return l.LargError(numArg, extraMsg);
+        });
+    }
+
+    public String LcheckString(int numArg) {
+        return lock(l -> {
+            return l.LcheckString(numArg);
+        });
+    }
+
+    public String LoptString(int numArg, String def) {
+        return lock(l -> {
+            return l.LoptString(numArg, def);
+        });
+    }
+
+    public double LcheckNumber(int numArg) {
+        return lock(l -> {
+            return l.LcheckNumber(numArg);
+        });
+    }
+
+    public double LoptNumber(int numArg, double def) {
+        return lock(l -> {
+            return l.LoptNumber(numArg, def);
+        });
+    }
+
+    public int LcheckInteger(int numArg) {
+        return lock(l -> {
+            return l.LcheckInteger(numArg);
+        });
+    }
+
+    public int LoptInteger(int numArg, int def) {
+        return lock(l -> {
+            return l.LoptInteger(numArg, def);
+        });
+    }
+
+    public void LcheckStack(int sz, String msg) {
+        lock(l -> {
+            l.LcheckStack(sz, msg);
+        });
+    }
+
+    public void LcheckType(int nArg, int t) {
+        lock(l -> {
+            l.LcheckType(nArg, t);
+        });
+    }
+
+    public void LcheckAny(int nArg) {
+        lock(l -> {
+            l.LcheckAny(nArg);
+        });
+    }
+
+    public int LnewMetatable(String tName) {
+        return lock(l -> {
+            return l.LnewMetatable(tName);
+        });
+    }
+
+    public void LgetMetatable(String tName) {
+        lock(l -> {
+            l.LgetMetatable(tName);
+        });
+    }
+
+    public void Lwhere(int lvl) {
+        lock(l -> {
+            l.Lwhere(lvl);
+        });
+    }
+
+    public int Lref(int t) {
+        return lock(l -> {
+            return l.Lref(t);
+        });
+    }
+
+    public void LunRef(int t, int ref) {
+        lock(l -> {
+            l.LunRef(t, ref);
+        });
+    }
+
+    public int LloadFile(String fileName) {
+        return lock(l -> {
+            return l.LloadFile(fileName);
+        });
+    }
+
+    public int LloadString(String s) {
+        return lock(l -> {
+            return l.LloadString(s);
+        });
+    }
+
+    public int LloadBuffer(byte[] buff, String name) {
+        return lock(l -> {
+            // Special case: Assuming LloadBuffer on 'l' still needs 'buff.length'
+            return l.LloadBuffer(buff, name);
+        });
+    }
+
+    public String Lgsub(String s, String p, String r) {
+        return lock(l -> {
+            return l.Lgsub(s, p, r);
+        });
+    }
+
+    //IMPLEMENTED C MACROS
+
+    public String LfindTable(int idx, String fname, int szhint) {
+        return lock(l -> {
+            return l.LfindTable(idx, fname, szhint);
+        });
+    }
+
+    public void pop(int n) {
+        lock(l -> {
+            l.pop(n);
+        });
+    }
+
+    public void getGlobal(String global) {
+        lock(l -> {
+            l.getGlobal(global);
+        });
+    }
+
+    public void setGlobal(String name) {
+        lock(l -> {
+            l.setGlobal(name);
+        });
+    }
+
+    // Functions to open lua libraries
+    public void openBase() {
+        lock(LuaState::openBase);
+    }
+
+    public void openTable() {
+        lock(LuaState::openTable);
+    }
+
+    public void openIo() {
+        lock(LuaState::openIo);
+    }
+
+    public void openOs() {
+        lock(LuaState::openOs);
+    }
+
+    public void openString() {
+        lock(LuaState::openString);
+    }
+
+    public void openMath() {
+        lock(LuaState::openMath);
+    }
+
+    public void openDebug() {
+        lock(LuaState::openDebug);
+    }
+
+    public void openPackage() {
+        lock(LuaState::openPackage);
+    }
+
+    /********************** Luajava API Library **********************/
+
+    public void openLibs() {
+        lock(LuaState::openLibs);
+    }
+
+    /**
+     * Gets a Object from Lua
+     *
+     * @param idx index of the lua stack
+     * @return Object
+     * @throws LuaException if the lua object does not represent a java object.
+     */
+    public Object getObjectFromUserdata(int idx) throws LuaException {
+        return lockThrow(l -> {
+           return l.getObjectFromUserdata(idx);
+        });
+    }
+
+    /**
+     * Tells whether a lua index contains a java Object
+     *
+     * @param idx index of the lua stack
+     * @return boolean
+     */
+    public boolean isObject(int idx) {
+        return lock(l -> {
+            return l.isObject(idx);
+        });
+    }
+
+    /**
+     * Pushes a Java Object into the lua stack.<br>
+     * This function does not check if the object is from a class that could
+     * be represented by a lua type. Eg: java.lang.String could be a lua string.
+     *
+     * @param obj Object to be pushed into lua
+     */
+    public void pushJavaObject(Object obj) {
+        lock(l -> {
+            l.pushJavaObject(obj);
+        });
+    }
+
+    public void pushJavaArray(Object obj) throws LuaException {
+        lockThrow(l -> {
+            l.pushJavaArray(obj);
+        });
+    }
+
+    /**
+     * Pushes a JavaFunction into the state stack
+     *
+     * @param func
+     */
+    public void pushJavaFunction(JavaFunction func) throws LuaException {
+        lockThrow(l -> {
+            l.pushJavaFunction(func);
+        });
+    }
+
+    /**
+     * Returns whether a userdata contains a Java Function
+     *
+     * @param idx index of the lua stack
+     * @return boolean
+     */
+    public boolean isJavaFunction(int idx) {
+        return lock(l -> {
+            return l.isJavaFunction(idx);
+        });
+    }
+
 }
